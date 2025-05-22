ac0bf86f6098b3216e96c0547b5a84b1b41dfde6
mat_dweb <mat.dweb28@gmail.com>
2025-05-21 21:34:37 -0600

    feat(Footer): add min-width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="border-t border-border py-10 bg-secondary/30">
+    <footer className="min-w-[550px] border-t border-border py-10 bg-secondary/30">
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           <div>
